1490e49d2172ebce43a480ae08641e2a8c403af9
alucadeil <alucadeil@mail.ru>
2021-02-11 16:59:37 +0300

    Changed templates to classes

--- src/classes/blocks.js
+++ src/classes/blocks.js
@@ -1,30 +1,54 @@
+import {col, css, row} from "../utils";
+
 class Block {
-    constructor(type, value, options) {
-        this.type = type
+    constructor( value, options) {
         this.value = value
         this.options = options
+    }
+
+    toHTML() {
+        throw new Error('Метод toHTML Должен быть реализован!')
     }
 }
 
 export class TitleBlock extends Block {
     constructor(value, options) {
-        super('title', value, options);
+        super(value, options);
+    }
+
+    toHTML() {
+
+        const {tag = 'h1', styles =''} = this.options
+        return row(col(`<${tag}>${this.value}<${tag}>`), css(styles));
     }
 }
 
 export class TextBlock extends Block {
     constructor(value, options) {
-        super('text', value, options);
+        super(value, options);
+    }
+
+    toHTML() {
+        return row(col(`<p>${this.value}</p>`))
     }
 }
 
 export class ColumnsBlock extends Block {
     constructor(value, options) {
-        super('columns', value, options);
+        super(value, options);
+    }
+
+    toHTML() {
+        const html = this.value.map(col).join('')
+        return row(html)
     }
 }
 export class ImageBlock extends Block {
     constructor(value, options) {
-        super('image', value, options);
+        super(value, options);
+    }
+
+    toHTML() {
+        return row(`<img src="${this.value}" alt="" style="width: 286px">`)
     }
 }
